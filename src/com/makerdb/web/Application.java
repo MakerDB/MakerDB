9dc500386e774f600e84d4b95c0f0320745f7486
Sushant Khanna <sushantkhanna1989@gmail.com>
2016-10-31 17:08:23 +0530

    Basic App setup done

---
+++ src/com/makerdb/web/Application.java
@@ -0,0 +1,16 @@
+package com.makerdb.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sukhanna on 10/28/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String args[]) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
